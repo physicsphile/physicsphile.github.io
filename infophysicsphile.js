f0888eea405f58ae567c4055a444ddee898dcab3
PhysicsPhile <64792123+physicsphile@users.noreply.github.com>
2020-05-13 22:24:42 +0530

    update infophysicsphile.js

--- infophysicsphile.js
+++ infophysicsphile.js
@@ -2,6 +2,6 @@ var username = 'admin';
 var password = 'admin';
 username=prompt('This is password protected page, please enter username to continue.','');
 password=prompt('This is password protected page, please enter password to continue.','');
-if (username != 'admin' || password != 'pass') {
+if (username != 'admin' || password != 'admin') {
 location.href='https://physics-phile.blogspot.com';
 }
