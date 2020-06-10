2c36f93ebdee94393ad63f53d3940ac128cdf300
PhysicsPhile <64792123+physicsphile@users.noreply.github.com>
2020-06-10 14:31:45 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","id12688118_ijeetregistration","-Ijeetregistration1");
+if (!$con)
+	{
+	die('could not connect; ' . myssqli_error());
+	}
+mysqli_select_db($con, "id12688118_registration",);
+?>	
+	
